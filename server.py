ade5633042f9c6fcc8891fd065bd9fa7abcbdb34
garavito16 <lex_94@outlook.com>
2022-01-21 13:40:14 -0500

    subir cambios

--- server.py
+++ server.py
@@ -8,7 +8,20 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    data = { 
+        "frutas" : {
+            "Strawberry" : request.form["strawberry"],
+            "Raspberry" : request.form["raspberry"],
+            "Apple" : request.form["apple"]
+        },
+        "info_user" : {
+            "first_name" : request.form["first_name"],
+            "last_name" : request.form["last_name"],
+            "student_id" : request.form["student_id"]
+        }
+    }
+    print(len(data["frutas"]))
+    return render_template("checkout.html",data=data)
 
 @app.route('/fruits')         
 def fruits():
